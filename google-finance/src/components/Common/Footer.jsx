e32ecdce6e3e63aa0c8ca88d603cf29e402e97a1
imeureka <lky062@naver.com>
2023-11-21 01:52:25 +0900

    design/#1 : Main Footer 간격 수정

--- google-finance/src/components/Common/Footer.jsx
+++ google-finance/src/components/Common/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import logoImage from '../../assets/img/image_logo.svg';
-import { FooterLogoImage } from '../../assets/index';
+import { FooterLogoImage, LineImage } from '../../assets/index';
 
 const Footer = () => {
   return (
@@ -12,7 +12,11 @@ const Footer = () => {
         <span>의견 보내기</span>
         <span>개인정보처리방침</span>
       </FooterHeader>
-      <span>google.com - 개인정보 보호</span>
+      <FooterSub>
+        <FooterText>google.com</FooterText>
+        <img src={LineImage} alt="글자 사이 라인" />
+        <FooterText>개인정보 보호</FooterText>
+      </FooterSub>
     </FooterContainer>
   );
 };
@@ -26,16 +30,23 @@ const FooterContainer = styled.nav`
 
   width: 99%;
   height: 115px;
-
   padding: 0 15px;
   bottom: 0;
 `;
 
+const FooterText = styled.span`
+  color: gray;
+`;
+
+const FooterSub = styled.div`
+  display: flex;
+`;
 const FooterHeader = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   width: 100%;
+  margin-bottom: 18px;
 `;
 export default Footer;
