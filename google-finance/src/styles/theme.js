6016fb855b1a5b284f8be7e86df2cb12e7c0c6c7
nayujin-dev <00skdbwls@gmail.com>
2023-11-21 01:43:17 +0900

    init/#2: theme의 font 세팅

--- google-finance/src/styles/theme.js
+++ google-finance/src/styles/theme.js
@@ -1,4 +1,5 @@
 import { css } from 'styled-components';
+import './font.css';
 
 const colors = {
   white: '#FFFFFF',
@@ -17,8 +18,143 @@ const colors = {
   gray_4: '#EFEFEF',
 };
 
+const fonts = {
+  productsans_10: css`
+    font-family: 'Product Sans';
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  productsans_12: css`
+    font-family: 'Product Sans';
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  productsans_14: css`
+    font-family: 'Product Sans';
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  productsans_14_bold: css`
+    font-family: 'Product Sans';
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  `,
+  productsans_16_cond: css`
+    font-family: 'Product Sans';
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 2.4rem;
+    letter-spacing: -0.08rem;
+  `,
+  productsans_18: css`
+    font-family: 'Product Sans';
+    font-size: 1.8rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  productsans_24: css`
+    font-family: 'Product Sans';
+    font-size: 2.4rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  productsans_32: css`
+    font-family: 'Product Sans';
+    font-size: 3.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+
+  roboto_10: css`
+    font-family: 'Roboto';
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  roboto_12: css`
+    font-family: 'Roboto';
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  roboto_12_bold: css`
+    font-family: 'Roboto';
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.06rem;
+  `,
+  roboto_14: css`
+    font-family: 'Roboto';
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  roboto_14_cond: css`
+    font-family: 'Roboto';
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.07rem;
+  `,
+  roboto_16: css`
+    font-family: 'Roboto';
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  roboto_18: css`
+    font-family: 'Roboto';
+    font-size: 1.8rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  roboto_18_bold: css`
+    font-family: 'Roboto';
+    font-size: 1.8rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.09rem;
+  `,
+  roboto_24: css`
+    font-family: 'Roboto';
+    font-size: 2.4rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  roboto_32: css`
+    font-family: 'Roboto';
+    font-size: 3.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+};
+
 const theme = {
   colors,
+  fonts,
 };
 
 export default theme;
