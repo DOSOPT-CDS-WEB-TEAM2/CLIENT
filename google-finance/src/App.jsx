28efb8fc84a33d9aa1355ddd0b2ddfcd861da6fd
nayujin-dev <00skdbwls@gmail.com>
2023-11-21 00:03:12 +0900

    init/#2: globalStyle, theme 세팅

--- google-finance/src/App.jsx
+++ google-finance/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
-
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/globalStyle';
+import theme from './styles/theme';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div></div>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
