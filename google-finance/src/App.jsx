d311557d3a6fb9843cc2e1a17b22a97fd937e10a
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-21 18:34:51 +0900

    feat: 버튼컨테이너(St.CommonButtonContainer)

--- google-finance/src/App.jsx
+++ google-finance/src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
-import { St } from './components/Common/buttons';
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <St.CommonButton>미국</St.CommonButton>
     </ThemeProvider>
   );
 }
--- google-finance/src/components/Common/buttons.jsx
+++ google-finance/src/components/Common/buttons.jsx
@@ -2,9 +2,16 @@ import styled from 'styled-components';
 import theme from '../../styles/theme';
 
 const St = {
+  CommonButtonContainer: styled.div`
+    display: flex;
+    align-items: center;
+
+    gap: 1rem;
+  `,
   CommonButton: styled.button`
     height: 3rem;
     padding: 0.8rem 1.4rem;
+    gap: 1rem;
 
     border-radius: 1.5rem;
     ${theme.fonts.roboto_12_cond};
