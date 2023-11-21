168be62f66479950108023e8b1945a43e792e905
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-21 17:59:36 +0900

    design: font roboto_12_cond 추가

--- google-finance/src/App.jsx
+++ google-finance/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
+import { St } from './components/Common/buttons';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <St.CommonButton>아시아</St.CommonButton>
     </ThemeProvider>
   );
 }
