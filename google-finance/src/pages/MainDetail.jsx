fbaa1ada3087260d965b0e278f14cba043a598a2
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-24 01:02:47 +0900

    feat: 필요한 컴포넌트 rough하게 분리

--- google-finance/src/pages/MainDetail.jsx
+++ google-finance/src/pages/MainDetail.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MainDetail = () => {
-  return <HomeWrapper>주가 상세페이지 입니다.</HomeWrapper>;
+  return (
+    <HomeWrapper>
+      주가 상세페이지 입니다.
+      <HeaderIndex />
+      <Title />
+      <Article /> {/* 여기 안에 title, price, graph, text */}
+      <Compare />
+      <InOutCome />
+    </HomeWrapper>
+  );
 };
 
 const HomeWrapper = styled.div`
