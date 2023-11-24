fa5a07d99854adabc0ddd66219fdab6d7d541c9d
nayujin-dev <00skdbwls@gmail.com>
2023-11-24 16:59:29 +0900

    chore/#6: 뉴스기사 더미데이터 추가

---
+++ google-finance/src/assets/data/ARTICLE_DUMMY.js
@@ -0,0 +1,37 @@
+export const ARTICLE_DUMMY = [
+  {
+    id: 1,
+    title: '공매도 전면 금지...갑자기 왜? (2023.11.05/뉴스데스크/MBC)',
+    imageUrl: null,
+    createdAt: '2023-11-20T17:26:26',
+    publisher: 'Youtube',
+  },
+  {
+    id: 2,
+    title: '“은퇴 후 시골에 집 짓고 살고 싶다... 전문가들은 뜯어 말리는 이유[왕개미연구소]',
+    imageUrl: null,
+    createdAt: '2023-11-20T17:27:07',
+    publisher: '조선일보',
+  },
+  {
+    id: 3,
+    title: '“전기차는 팔수록 적자" 수요 둔화에 이차 전지 안갯속으로',
+    imageUrl: null,
+    createdAt: '2023-11-20T17:27:09',
+    publisher: '동아일보',
+  },
+  {
+    id: 4,
+    title: '‘캐시미어 30%’ 가짜였다... 6년간 소비자 기만한 유명 쇼핑몰 머플러',
+    imageUrl: null,
+    createdAt: '2023-11-20T17:27:11',
+    publisher: '문화일보',
+  },
+  {
+    id: 6,
+    title: '5년여간 KTX ・SRT등 여객열차 부정승차 164만건...하루 800건',
+    imageUrl: null,
+    createdAt: '2023-11-20T08:29:42',
+    publisher: 'MBC뉴스',
+  },
+];
