3b2240819529a1d16a008a64302a09f23a09b7fb
nayujin-dev <00skdbwls@gmail.com>
2023-11-24 18:33:19 +0900

    feat/#6: 뉴스기사 이미지 있는경우와 없는경우 구분해서 구현

--- google-finance/src/components/Common/NewsArticle.jsx
+++ google-finance/src/components/Common/NewsArticle.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const NewsArticle = ({ article }) => {
-  //   const [imgUrl, setImgUrl] = useState('');
-  const { title, imgUrl, createdAt, publisher } = article;
+  const { title, imageUrl, createdAt, publisher } = article;
   return (
     <St.NewsArticleContainer>
-      <St.ArticleEtcInfo>{publisher} | 2시간 전</St.ArticleEtcInfo>
-      <St.ArticleTitle>{title}</St.ArticleTitle>
-      {imgUrl && <St.ArticlePictureWrapper></St.ArticlePictureWrapper>}
+      <St.ArticleWrapper imageUrl={imageUrl}>
+        <St.ArticleEtcInfo>{publisher} | 2시간 전</St.ArticleEtcInfo>
+        <St.ArticleTitle>{title}</St.ArticleTitle>
+      </St.ArticleWrapper>
+      {imageUrl && (
+        <St.ArticlePictureWrapper>
+          <img src={imageUrl} alt="뉴스기사-사진" />
+        </St.ArticlePictureWrapper>
+      )}
     </St.NewsArticleContainer>
   );
 };
@@ -18,13 +23,21 @@ export default NewsArticle;
 const St = {
   NewsArticleContainer: styled.div`
     display: flex;
+    border-bottom: 0.05rem solid;
+    /* border-bottom-width: 34.4rem; */
+    border-bottom-color: ${({ theme }) => theme.colors.gray_3};
+  `,
+  ArticleWrapper: styled.div`
+    display: flex;
     justify-content: center;
     align-items: flex-start;
     flex-direction: column;
     flex-shrink: 0;
 
+    width: ${({ imageUrl }) => (imageUrl !== null ? '29rem' : '37.5rem')};
     height: 9.9rem;
-    padding: 1rem 1.4rem 0.9rem 1.6rem;
+
+    padding: ${({ imageUrl }) => (imageUrl !== null ? '1rem 1rem 0.9rem 1.6rem' : '1rem 1.4rem 0.9rem 1.6rem')};
     gap: 1rem;
   `,
   ArticleTitle: styled.div`
@@ -35,5 +48,11 @@ const St = {
     color: ${({ theme }) => theme.colors.gray_2};
     ${({ theme }) => theme.fonts.roboto_12};
   `,
-  ArticlePictureWrapper: styled.div``,
+  ArticlePictureWrapper: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-right: 1.5rem;
+  `,
 };
