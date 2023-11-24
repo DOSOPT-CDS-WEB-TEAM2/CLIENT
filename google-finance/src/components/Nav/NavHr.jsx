a67561b5c69dee07c8c72ebf5c52cb0556ef9e57
imeureka <lky062@naver.com>
2023-11-24 21:41:41 +0900

    chore/#9 : css 속성 순서

--- google-finance/src/components/Nav/NavHr.jsx
+++ google-finance/src/components/Nav/NavHr.jsx
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 const NavHr = styled.hr`
   position: relative;
+
+  width: 25rem;
   left: -1.5rem;
   margin: 0;
-  width: 25rem;
+
   border: none;
   border-top: 0.05rem solid ${(props) => props.theme.colors.gray_3};
   margin-bottom: ${(props) => props.marginBottom};
