eec7f88cd67688ccf43f61573a3b07d9b27d51c8
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-25 00:50:48 +0900

    feat/#10 : STOCKINFO_DUMMY 매핑

--- google-finance/src/components/Detail/Graph/InfoText.jsx
+++ google-finance/src/components/Detail/Graph/InfoText.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import InfoEach from './InfoEach';
+import { STOCKINFO_DUMMY } from '../../../assets/data/STOCKINFO_DUMMY';
 
 const InfoText = () => {
   return (
     <St.Container>
-      <InfoEach category="전일종가" value="₩471,000.00" />
-      <InfoEach category="일일 변동 폭" value="₩487,500.00  -  ₩529,000.00" />
-      <InfoEach category="52주 변동폭" value="₩424,500.00  -  ₩857,000.00" />
-      <InfoEach category="시가 총액" value="38.70조 KRW" />
-      <InfoEach category="평균 거래량" value="31.74만" />
-      <InfoEach category="주가 수익률" value="26.73%" />
-      <InfoEach category="배당 수익률" value="1.95%" />
+      {STOCKINFO_DUMMY.map((info) => (
+        <InfoEach key={info.id} category={info.category} value={info.value} />
+      ))}
     </St.Container>
   );
 };
