7e195f4861d2cb04902dd76ee8f98700a82ee140
imeureka <lky062@naver.com>
2023-11-26 16:26:29 +0900

    chore/#9 : 아이콘 변경 arrow 크기 변경

--- google-finance/src/assets/index.jsx
+++ google-finance/src/assets/index.jsx
@@ -16,3 +16,4 @@ export { default as Share } from './icon/share.svg';
 export { default as Check } from './icon/ic_check.svg';
 export { default as GraphRedImage } from './img/graph_red.png';
 export { default as ArrowUpIcon } from './icon/ic_arrow_up.svg';
+export { default as ArrowUpSmallIcon } from './icon/ic_arrow_up_small.svg';
