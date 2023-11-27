d77a948456bcfbc07cdd55d1d18d39bbad1451d1
imeureka <lky062@naver.com>
2023-11-28 03:19:10 +0900

    chore/#17 : 그래프이미지, 버튼 아이콘 추가 ,더미데이터 추가 및 bold체 추가

--- google-finance/src/assets/data/index.js
+++ google-finance/src/assets/data/index.js
@@ -30,3 +30,42 @@ export const SIDE_DUMMY = [
     fluctuationRate: 8.7,
   },
 ];
+
+export const MARKET_STATUS_USA = [
+  {
+    id: 1,
+    name: 'S&P 500',
+    currentStockIndex: '31,949.89',
+    fluctuationRate: -0.69,
+  },
+  {
+    id: 2,
+    name: '다우 존스 산업평균지수',
+    currentStockIndex: '3,030.80',
+    fluctuationRate: -0.45,
+  },
+  {
+    id: 3,
+    name: '나스닥 종합주가지수',
+    currentStockIndex: '17,664.12',
+    fluctuationRate: -1.59,
+  },
+  {
+    id: 4,
+    name: 'BSE 센섹스',
+    currentStockIndex: '65,363.78',
+    fluctuationRate: -1.59,
+  },
+  {
+    id: 5,
+    name: 'BSE 센섹스',
+    currentStockIndex: '65,363.78',
+    fluctuationRate: -1.59,
+  },
+  {
+    id: 6,
+    name: 'BSE 센섹스',
+    currentStockIndex: '65,363.78',
+    fluctuationRate: -1.59,
+  },
+];
--- google-finance/src/assets/index.jsx
+++ google-finance/src/assets/index.jsx
@@ -15,5 +15,7 @@ export { default as BtnRight } from './icon/btn_right.svg';
 export { default as Share } from './icon/share.svg';
 export { default as Check } from './icon/ic_check.svg';
 export { default as GraphRedImage } from './img/graph_red.png';
+export { default as GraphRed3xImage } from './img/graph_red3x.svg';
 export { default as ArrowUpIcon } from './icon/ic_arrow_up.svg';
 export { default as ArrowUpSmallIcon } from './icon/ic_arrow_up_small.svg';
+export { default as BtnRightIcon } from './icon/btn_right_mar.svg';
