a04399dde041ca20c9e4d6d789e95ac486ce2a3c
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-28 17:47:05 +0900

    feat/#10 : 비교대상 헤더

--- google-finance/src/components/Detail/Compare/CompareHeader.jsx
+++ google-finance/src/components/Detail/Compare/CompareHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SearchIcon } from '../../../assets';
+import theme from '../../../styles/theme';
 
 const CompareHeader = () => {
   return (
     <St.Container>
-      <img src={SearchIcon} />
+      <img src={SearchIcon} /> 비교대상
     </St.Container>
   );
 };
@@ -15,9 +16,15 @@ export default CompareHeader;
 const St = {
   Container: styled.div`
     display: flex;
+    align-items: center;
+    gap: 1rem;
 
     width: 100%;
     height: 4.8rem;
     margin-top: 2rem;
+    padding-left: 1.5rem;
+
+    color: ${theme.colors.blue_main};
+    ${theme.fonts.roboto_18};
   `,
 };
