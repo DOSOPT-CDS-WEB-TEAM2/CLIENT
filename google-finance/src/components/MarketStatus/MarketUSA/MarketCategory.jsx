19620281fa7b26e538f5ccfa8109341c5b4085fd
imeureka <lky062@naver.com>
2023-11-29 05:33:04 +0900

    feat/#17 : market 시장 카테고리 컴포넌트 props

--- google-finance/src/components/MarketStatus/MarketUSA/MarketCategory.jsx
+++ google-finance/src/components/MarketStatus/MarketUSA/MarketCategory.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { BtnRightIcon } from '../../../assets';
 
-const MarketCategory = () => {
+const MarketCategory = ({ category }) => {
   return (
     <St.Container>
-      <St.Title>미주</St.Title>
-      <img src={BtnRightIcon} alt="미주-카테고리-더보기" />
+      <St.Title>{category}</St.Title>
+      <img src={BtnRightIcon} alt={`${category}-카테고리-더보기`} />
     </St.Container>
   );
 };
@@ -20,7 +20,9 @@ const St = {
 
     width: 100%;
     height: 4.8rem;
+
     padding: 0.9rem 1.3rem;
+    border-bottom: solid 0.2rem ${(props) => props.theme.colors.gray_4};
 
     img {
       width: 2.4rem;
--- google-finance/src/pages/MarketStatus.jsx
+++ google-finance/src/pages/MarketStatus.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import theme from '../styles/theme';
 import HeaderIndex from '../components/MarketStatus/HeaderIndex/HeaderIndex';
 import MarketCategory from '../components/MarketStatus/MarketUSA/MarketCategory';
 import MarketUSA from '../components/MarketStatus/MarketUSA/MarketUSA';
-import NavHr from '../components/Nav/NavHr';
 import { MARKET_STATUS_USA } from '../assets/data';
+
+const USA = '미주';
+const MID_EAST_AFRICA = '유럽, 중동, 아프리카';
 
 const MarketStatus = () => {
   const MARKET = MARKET_STATUS_USA;
   return (
     <HomeContainer>
       <HeaderIndex />
-      <MarketCategory />
+      <MarketCategory category={USA} />
 
       {MARKET.map((MARKET, index) => (
         <div key={index}>
-          <hr />
+          <MarketHr />
           <MarketUSA
             title={MARKET.name}
             stockIndex={MARKET.currentStockIndex}
@@ -23,7 +26,8 @@ const MarketStatus = () => {
           />
         </div>
       ))}
-       <MarketCategory />
+
+      <MarketCategory category={MID_EAST_AFRICA} />
     </HomeContainer>
   );
 };
@@ -32,6 +36,13 @@ const HomeContainer = styled.section`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
+`;
+
+const MarketHr = styled.hr`
+  position: relative;
+  width: 92%;
+  margin: 0 auto;
+  border-top: 0.03rem solid ${(props) => props.theme.colors.gray_4};
 `;
 
 export default MarketStatus;
