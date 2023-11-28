00f052024884b8aa5c8dc5ec38258b3759431639
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-28 18:18:45 +0900

    refactor : price,price detail className -> styled-components

--- google-finance/src/components/Detail/MainInfo/Price.jsx
+++ google-finance/src/components/Detail/MainInfo/Price.jsx
@@ -5,8 +5,8 @@ import theme from '../../../styles/theme';
 const Price = () => {
   return (
     <St.Container>
-      <div className="price">₩512,000.00</div>
-      <div className="price_detail">11월 6일, 오후 12시 45분 32초 UTC+9 KRW KRX 면책조항</div>
+      <St.Price>₩512,000.00</St.Price>
+      <St.PriceDetail>11월 6일, 오후 12시 45분 32초 UTC+9 KRW KRX 면책조항</St.PriceDetail>
     </St.Container>
   );
 };
@@ -15,26 +15,20 @@ const St = {
   Container: styled.div`
     display: flex;
     flex-direction: column;
-
     width: 100%;
     height: 9rem;
     padding: 0 1.5rem;
-
     border: 0.05rem solid ${theme.colors.gray_3};
-
-    .price {
-      margin-top: 1.2rem;
-      margin-bottom: 0.5rem;
-
-      ${theme.fonts.productsans_32};
-    }
-
-    .price_detail {
-      margin-top: 0;
-
-      color: ${theme.colors.black};
-      ${theme.fonts.roboto_12_cond};
-    }
+  `,
+  Price: styled.div`
+    margin-top: 1.2rem;
+    margin-bottom: 0.5rem;
+    ${theme.fonts.productsans_32};
+  `,
+  PriceDetail: styled.div`
+    margin-top: 0;
+    color: ${theme.colors.black};
+    ${theme.fonts.roboto_12_cond};
   `,
 };
 
