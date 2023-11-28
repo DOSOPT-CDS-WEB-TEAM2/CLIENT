800f5e5051f7acae65d0df19c1cdf72a7334454d
imeureka <lky062@naver.com>
2023-11-28 15:28:45 +0900

    feat/#17 : market시장 살펴보기 -header

---
+++ google-finance/src/components/MarketStatus/HeaderIndex/HeaderIndex.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CommonButton, CommonButtonContainer } from '../../Common/buttons';
+import NavHr from '../../Nav/NavHr';
+
+const HeaderIndex = () => {
+  const buttonValues = [
+    '📊시장지수',
+    '⬆️가장 거래가 활발한 주식',
+    '📈상승주',
+    '📉하락주',
+    '🌱기후변화 대응 선두 업체',
+    '🪙암호화폐',
+    '💰통화',
+  ];
+
+  return (
+    <St.Container>
+      <St.Title>시장현황 살펴보기</St.Title>
+      <CommonButtonContainer>
+        {buttonValues.map((value, index) => (
+          <CommonButton key={index}>
+            <span>{value}</span>
+          </CommonButton>
+        ))}
+      </CommonButtonContainer>
+      <NavHr marginBottom="1rem" width="100%" />
+    </St.Container>
+  );
+};
+
+const St = {
+  Container: styled.header`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 0.9rem 1.3rem;
+  `,
+  Title: styled.h1`
+    margin-bottom: 1.5rem;
+    ${(props) => props.theme.fonts.roboto_24};
+  `,
+};
+
+export default HeaderIndex;
