6bb3f8334b5a85e853e2385689dc8dbf9a7ddb8e
imeureka <lky062@naver.com>
2023-12-01 21:29:05 +0900

    refactor/#18 : 불필요한 div 제거 및 theme 추가

--- google-finance/src/components/Nav/ActiveStock/ActiveStockContainer.jsx
+++ google-finance/src/components/Nav/ActiveStock/ActiveStockContainer.jsx
@@ -4,17 +4,15 @@ import styled from 'styled-components';
 import theme from '../../../styles/theme';
 
 const ActiveStockContainer = () => (
-  <div>
-    <St.ActiveStockContainer>
-      <St.NavTitle>가장 거래가 활발한 주식</St.NavTitle>
-      <StockList />
-    </St.ActiveStockContainer>
-  </div>
+  <St.ActiveStockContainer>
+    <St.NavTitle>가장 거래가 활발한 주식</St.NavTitle>
+    <StockList />
+  </St.ActiveStockContainer>
 );
 
 const St = {
   ActiveStockContainer: styled.div`
-    padding: 0 0 1rem 0;
+    padding-bottom: 1rem;
   `,
 
   NavTitle: styled.div`
