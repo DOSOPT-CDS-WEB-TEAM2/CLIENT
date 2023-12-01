c7834a71af93423181ca0e8a1bda63ad77b999a6
Nayujin <00nyj@sookmyung.ac.kr>
2023-12-01 21:15:08 +0900

    chore/#33: 포트폴리오 더미데이터 분리

---
+++ google-finance/src/assets/data/PORTFOLIO_DUMMY.js
@@ -0,0 +1,18 @@
+export const PORTFOLIO_DUMMY = [
+  {
+    id: 1,
+    name: '051910',
+    company: 'LG화학',
+    imgUrl: null,
+    price: '\\512,000.00',
+    percentage: 8.7,
+  },
+  {
+    id: 2,
+    name: 'APPL',
+    company: '애플',
+    imgUrl: null,
+    price: '$182.41',
+    percentage: -0.26,
+  },
+];
