15170afb767e19d11c7ebf3bab83cf3fb69459ad
Nayujin <00nyj@sookmyung.ac.kr>
2023-12-01 21:29:24 +0900

    fix: 배포 api 오류 수정

--- google-finance/src/components/Api/FetchData.js
+++ google-finance/src/components/Api/FetchData.js
@@ -20,3 +20,13 @@ export const FetchArticleData = async () => {
     throw error; // 에러를 상위로 전파
   }
 };
+
+export const FetchMarketData = async () => {
+  try {
+    const response = await API.get(`/stock-index`);
+    return response.data;
+  } catch (error) {
+    console.error('Error during fetch:', error);
+    throw error; // 에러를 상위로 전파
+  }
+};
